ac41aa7ce861d557a45fce1f6a9a6ec1d16a498d
karthikperiyasamy-tp <karthikperiyasamy.tp@gmail.com>
2026-03-12 12:16:30 +0530

    a14

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Performance optimizations
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
+    removeConsole: false,
   },
   
   // Optimize images
