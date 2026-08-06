746db253151d060cb6e6f1f53874f909795999a4
karthikperiyasamy-tp <karthikperiyasamy.tp@gmail.com>
2026-08-06 19:39:10 +0530

    Update AdScript.tsx

--- src/app/components/AdScript.tsx
+++ src/app/components/AdScript.tsx
@@ -6,26 +6,26 @@ export default function AdScript() {
   useEffect(() => {
     if (document.getElementById("AdsCoreLoader101206")) return;
 
-    const container = document.createElement("div");
-    container.className = "ads-core-ads";
-    document.body.appendChild(container);
+    // const container = document.createElement("div");
+    // container.className = "ads-core-ads";
+    // document.body.appendChild(container);
 
-    const s = document.createElement("script");
-    s.src =
-      "https://sads.adsboosters.xyz/6b25da1d7c79a47dd21b1764379d56a3.js";
-    s.id = "AdsCoreLoader101206";
-    s.type = "text/javascript";
-    s.setAttribute("data-cfasync", "false");
-    document.body.appendChild(s);
+    // const s = document.createElement("script");
+    // s.src =
+    //   "https://sads.adsboosters.xyz/6b25da1d7c79a47dd21b1764379d56a3.js";
+    // s.id = "AdsCoreLoader101206";
+    // s.type = "text/javascript";
+    // s.setAttribute("data-cfasync", "false");
+    // document.body.appendChild(s);
 
-    // Monetag In-Page Push
-    if (!document.getElementById("monetag-inpage")) {
-      const monetag = document.createElement("script");
-      monetag.id = "monetag-inpage";
-      monetag.dataset.zone = "11275969";
-      monetag.src = "https://nap5k.com/tag.min.js";
-      document.body.appendChild(monetag);
-    }
+    // // Monetag In-Page Push
+    // if (!document.getElementById("monetag-inpage")) {
+    //   const monetag = document.createElement("script");
+    //   monetag.id = "monetag-inpage";
+    //   monetag.dataset.zone = "11275969";
+    //   monetag.src = "https://nap5k.com/tag.min.js";
+    //   document.body.appendChild(monetag);
+    // }
   }, []);
 
   return null;
